fe321ff80c01e56314c0e8879b65350ded5e6e6e
rame3519 <sandeepramesh@ou.edu>
2025-10-28 23:35:18 -0500

    Update regression-algo.py

--- backend/Regression-Task/regression-algo.py
+++ backend/Regression-Task/regression-algo.py
@@ -23,7 +23,7 @@ def build_arg_parser() -> argparse.ArgumentParser:
     p.add_argument("--salaries-csv-in",  type=str, default="datasets/archive/jobs/salaries.csv", help="path to read salaries CSV")
 
     # -- Output CSV Columns -- 
-    p.add_argument("--out-csv-cols",  type=str, nargs='+', default=['r_squared', 'mean_squared_error', 'f_statistics', 'p_values', 'confidence_bounds'], help="path to save CSV output of regression algo analysis")
+    p.add_argument("--out-csv-cols",  type=str, nargs='+', default=['mean_squared_error', 'r2_score', 'explained_variance_score', 'max_error', 'confusion_matrix', 'f1_score'], help="path to save CSV output of regression algo analysis")
 
     # -- Output Path -- 
     p.add_argument("--out-csv",  type=str, default="datasets/output/regression-output0.csv", help="path to save CSV output of regression analysis")
@@ -72,6 +72,7 @@ if __name__ == "__main__":
         parser.set_defaults(**cfg_defaults)
 
     args = parser.parse_args()
+
 
 
 
