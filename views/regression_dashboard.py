1382ae44c731efa16e41c60ea092435ae57a6552
Sandeep Ramesh <sandeepramesh94@gmail.com>
2025-11-14 14:24:41 -0600

    file modifictaions

--- views/regression_dashboard.py
+++ views/regression_dashboard.py
@@ -23,9 +23,32 @@ st.title("Salary Predictor — Estimate Pay from Role, Company & Skills")
 #*Note:* This is an estimate from historical postings—actual compensation may vary.
 #""")
 
-# Get all skills
+# Get all skills and get the entire dataframe for all the skills.
 skill_cols = set(st.session_state.job_market_filtered_df.columns[31:])
 skill_cols.add('normalized_salary')
+skill_df = st.session_state.job_market_filtered_df.loc[:, st.session_state.job_market_filtered_df.columns.isin(skill_cols)]
+
+# Get dataframe of the entire dataset
+entire_df = st.session_state.job_market_filtered_df
+
+
+# Extract the columns for the regression.
+job_title_column = entire_df['title']
+company_column = entire_df['company_name']
+zip_code_column = entire_df['zip_code']
+job_location_column = entire_df['location']
+normalized_salary_column = entire_df['normalized_salary']
+max_salary_column = entire_df['max_salary']
+med_salary_column = entire_df['med_salary']
+min_salary_column = entire_df['min_salary']
+
+# Get a unique list of the job title, company, zip code, and the location for the user input.
+job_title_list = list(set(job_title_column.tolist()))
+company_list = list(set(company_column.tolist()))
+zip_code_list = list(set(zip_code_column.tolist()))
+job_location_list = list(set(job_location_column.tolist()))
+
+
 
 # page set up for regression
 st.set_page_config(page_title="Job Regression Dashboard", layout="wide")
@@ -39,23 +62,27 @@ tabs = st.tabs(
 with tabs[0]:
     # set title of the tab.
     st.header("Regression Inputs")
-    col1, col2, col3, col4 = st.columns(4) # Set up 4 columns for each input.
+    col1, col2, col3, col4, col5 = st.columns(5) # Set up 4 columns for each input.
 
     # Job skill for column 1.
     with col1:
-        skill = st.selectbox("Type of Skill", skill_cols)
+        skill_input = st.selectbox("Type of Skill", sorted(skill_cols))
 
     # Company for column 2.
     with col2:
-        company = st.selectbox("Company Name", ["Boeing", "Northrop Grumman", "Microsoft"])
+        company_input = st.selectbox("Company Name", company_list)
 
     # Job title for column 3.
     with col3:
-        job_title = st.selectbox("Job Title", ["Manager", "Software Tester", "Quality Control"])
+        job_title_input = st.selectbox("Job Title", sorted(job_title_list))
 
     # Zip code for column 4.
     with col4:
-        zip_code = st.number_input("Zip Code", 11111, 99999, 73019, step=1)
+        zip_code_input = st.selectbox("Zip Code", sorted(zip_code_list))
+
+    # location for column 5.
+    with col5:
+        location_input = st.selectbox("Location", sorted(job_location_list))
 
 with tabs[1]:
     st.header("Job Data Scatterplot")
